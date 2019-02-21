3afe73e2d8a974be7a34d29b400a2bfa86916b65
hwanjin.jeong <hwanjin.jeong@gmail.com>
2019-02-21 20:36:07 +0900

    Add files for ui

---
+++ configs/expand-region.el
@@ -0,0 +1,4 @@
+(use-package expand-region
+  :ensure t
+  :bind (("C-j" . er/expand-region)))
+
---
+++ profiles/windows.el
@@ -0,0 +1,38 @@
+;;; package --- Summary"
+;;; Code:
+;;; Commentary:
+;;; setup theme, key mappings, and windows
+
+;;; theme
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t
+  :init
+  (load-theme 'sanityinc-tomorrow-bright t))
+
+;; remove top menu bar
+(menu-bar-mode -1)
+
+;;; key mappings
+(load-config "windmove")
+(load-config "expand-region")
+(load-config "helm")
+
+(global-set-key (kbd "C-c C-n") 'next-buffer)
+(global-set-key (kbd "C-c C-p") 'previous-buffer)
+
+
+
+;; share osx clipboard
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
+;; linum config
+;; (require 'linum-relative)
+;; (linum-relative-global-mode t)
