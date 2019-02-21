1c269a76ad2869b8d9051da4172e82c3ba336cbf
hwanjin.jeong <hwanjin.jeong@gmail.com>
2019-02-21 20:34:34 +0900

    Modify default file

--- profiles/default.el
+++ profiles/default.el
@@ -1,8 +1,16 @@
-;;; setup: package archives with melpa
+;;; package --- Summary"
+;;; Code:
+;;; Commentary:
+;;; setup package archives with melpa
+
 (require 'package)
 (set-default-coding-systems 'utf-8)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 ;;;(package-refresh-contents) uncomment this line to solve the package not found problem
@@ -11,24 +19,5 @@
 (when (not (package-installed-p 'use-package))
   (package-install 'use-package))
 (require 'use-package)
-
-(load-conf-file "ibuffer")
-(load-conf-file "windmove")
-
-(load-conf-file "git")
-
-(load-conf-file "company")
-(load-conf-file "flycheck")
-
-(load-conf-file "helm")
-(load-conf-file "helm-swoop")
-(load-conf-file "ag")
-(load-conf-file "helm-ag")
-(load-conf-file "projectile")
-
-(load-conf-file "dockerfile")
-(load-conf-file "yaml")
-(load-conf-file "json")
-
 
 (provide 'default)
