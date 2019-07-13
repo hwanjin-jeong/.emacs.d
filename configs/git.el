d0f81c2614f5efea32396c3f3eca428f9f105990
Hwanjin Jeong <hwanjin.jeong@gmail.com>
2019-07-13 21:43:28 +0900

    modify magit option

--- configs/git.el
+++ configs/git.el
@@ -1,9 +1,14 @@
 
 (use-package magit
   :ensure t
-  :defer t)
+  :defer t
+  :bind (([f8] . magit-status))
+  :init (use-package magit-gitflow
+	  :ensure t
+	  :defer t ))
 
-(global-set-key [f8] 'magit-status)
+;; (global-set-key [f8] 'magit-status)
 ;; (global-set-key (kbd "M-g s") 'magit-status)
 ;; (global-set-key (kbd "M-g c") 'magit-commit)
 ;; (global-set-key (kbd "M-g p") 'magit-push)
+(add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
