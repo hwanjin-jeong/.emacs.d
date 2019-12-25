0c5a8ad0c200085996d93494a1bb405bfb16c0b8
Hwanjin Jeong <hwanjin.jeong@gmail.com>
2019-12-26 01:59:03 +0900

    modify shortcut

--- configs/helm.el
+++ configs/helm.el
@@ -31,7 +31,7 @@
 
     (global-set-key (kbd "M-x") 'helm-M-x)
     (global-set-key (kbd "M-s") 'helm-do-ag-this-file)
-    (global-set-key (kbd "C-x b") 'helm-buffers-list)
+    (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
     (global-set-key (kbd "C-x C-f") 'helm-find-files)
     (global-set-key (kbd "C-c r") 'helm-recentf)
     (global-set-key (kbd "M-y") 'helm-show-kill-ring)
